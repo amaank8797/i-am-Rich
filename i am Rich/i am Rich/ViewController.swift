c70cc40cd04619f5c0ad728b72006eb9bbb493b6
Amaan <96060017+amaank8797@users.noreply.github.com>
2023-01-10 00:33:56 +0530

    Add files via upload

---
+++ i am Rich/i am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am Rich
+//
+//  Created by Amaan Amaan on 16/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
